57118f317869886b0d64180813cf5cf692247b3f
Ali Mihandoost <i@ali.md>
2015-12-22 20:50:24 +0330

    userlist: add simple enterUser

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,5 +3,12 @@ angular
 
 .controller('UserListCtrl', function($scope, UserStrorage) {
   $scope.userList = UserStrorage.getUserList();
+
+  $scope.userEnter = function (user) {
+    console.log('userEnter: %o', user);
+    user.enter = {
+      timeString: '00:00 am'
+    };
+  };
 })
 ;
